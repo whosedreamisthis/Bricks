7ffbbc880fc12b19c5fedc9b8d1ee7aa7853b4b1
Dana Sharon <whosedreamisthis@gmail.com>
2023-05-26 13:31:39 -0700

    follow paddle when ball not in play

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] float speed = 500;
+    [SerializeField] Transform paddle;
+    public bool inPlay = false;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.up * speed);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        if (!inPlay)
+        {
+            transform.position = paddle.position + transform.up * 0.5f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                inPlay = true;
+                rb.AddForce(Vector2.up * speed);
+            }
+        }
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bottom"))
+        {
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+
 }
